b85efa04e95a21081359b4d0b6abf1627778aa18
kiran <kiransalian91gmail.com>
2018-08-06 20:26:30 +0530

    first commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TimeAgoPipe } from 'time-ago-pipe';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [TimeAgoPipe],
+  exports: [TimeAgoPipe]
+})
+export class SharedModule { }
